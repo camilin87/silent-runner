1fe31846161a91f464328a89cc11a78134ff789f
Camilo Sanchez <camilin87@gmail.com>
2016-10-11 16:57:11 -0400

    log the arguments

--- SilentRunner/Program.cs
+++ SilentRunner/Program.cs
@@ -9,17 +9,13 @@ namespace SilentRunner
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //            Application.Run(new Form1());
 
-            EventLog.WriteEntry("System", "SilentRunner Starting");
+            EventLog.WriteEntry("System", $"SilentRunner; Action=Start; Args={string.Join(" ", args)}");
         }
     }
 }
