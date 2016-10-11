d85d525740b0afacbae88943e64beafc34dd0387
Camilo Sanchez <camilin87@gmail.com>
2016-10-11 16:51:43 -0400

    create the silent runner application

---
+++ SilentRunner/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SilentRunner
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //            Application.Run(new Form1());
+
+            EventLog.WriteEntry("System", "SilentRunner Starting");
+        }
+    }
+}
